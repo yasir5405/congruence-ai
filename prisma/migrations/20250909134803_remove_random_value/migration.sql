c6ea963080307681e8c2178972c39cbe39f8db2f
yasir5405 <yasirnaseem5415@gmail.com>
2025-09-09 19:22:10 +0530

    remove redundant cols from tables

---
+++ prisma/migrations/20250909134803_remove_random_value/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `randomValue` on the `Users` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."Users" DROP COLUMN "randomValue";
