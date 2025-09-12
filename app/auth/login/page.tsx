2ba16cfd95dc05c382bc914dd9ecb0b68eecf94d
yasir5405 <yasirnaseem5415@gmail.com>
2025-09-12 19:12:12 +0530

    remove suspense from login form

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,26 +1,27 @@
-"use client";
 import { GalleryVerticalEnd } from "lucide-react";
 
 import { LoginForm } from "@/components/login-form";
-import { Suspense } from "react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/authOptions";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect("/dashboard");
+  }
   return (
-    <Suspense fallback={<p>Loading...</p>}>
-      <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-        <div className="flex w-full max-w-sm flex-col gap-6">
-          <a
-            href="#"
-            className="flex items-center gap-2 self-center font-medium"
-          >
-            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-              <GalleryVerticalEnd className="size-4" />
-            </div>
-            Congreunce AI
-          </a>
-          <LoginForm />
-        </div>
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <GalleryVerticalEnd className="size-4" />
+          </div>
+          Congreunce AI
+        </a>
+        <LoginForm />
       </div>
-    </Suspense>
+    </div>
   );
 }
